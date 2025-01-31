d91e4681aaf09a7defd245a07c5ef5b5b9b9ec92
annilla2 <anilanuredini92@gmail.com>
2025-01-31 05:46:56 -0800

    ndryshime tek komentet

--- read.js
+++ read.js
@@ -1,13 +1,13 @@
 document.getElementById('read-more').addEventListener('click', function(event) {
-    event.preventDefault(); // Parandalon veprimin e parazgjedhur të linkut
+    event.preventDefault(); 
 
     var moreText = document.getElementById('more-text');
 
     if (moreText.style.display === 'none') {
-        moreText.style.display = 'block'; // Shfaq tekstin e mëtejshëm
+        moreText.style.display = 'block'; // Shfaq tekstin 
         this.textContent = 'Read Less'; // Ndryshon tekstin e butonit
     } else {
-        moreText.style.display = 'none'; // Fshih tekstin e mëtejshëm
-        this.textContent = 'Read More'; // Kthehet teksti i mëparshëm i butonit
+        moreText.style.display = 'none'; // Fshih tekstin 
+        this.textContent = 'Read More'; // Kthehet teksti i butonit
     }
 });

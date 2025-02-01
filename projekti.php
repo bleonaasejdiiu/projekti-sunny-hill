ae08acf1c42ce5bf137e35ccf9e57d338e4fafee
bleonaasejdiiu <bleonasejdiu51@gmail.com>
2025-02-01 17:38:24 +0100

    css i rregulluar

--- projekti.php
+++ projekti.php
@@ -197,41 +197,17 @@
 
 </div> 
 
-<!--<div class="footer">
-            
-    <div class="footer-item1"> 
-         <p>Volunteer</p>
-       <p>Privacy Policy</p>
-        <p>Terms Of Use</p>
 
+<style>
+    .footer {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 20px;
+        background-color: rgb(240, 233, 237); /* Ngjyra e qumështit */
+        color: #333;
+    }
 
-    </div>
-    
-
-
-   <div class="footer-item2">
-   <p>Email: info@sunnyhillfestival.com</p>
-   <p>Sunny Hill Festival</p>
-   <p>Enver Maloku, Nr.82</p>
-   <p>Pristina 10000 Kosove</p>
-   
-    </div>
-
-    <div class="footer-item">
-        <h3>Sponsors</h3>
-     <img src="klankosova.png">
-      <img src="cocacola.jpg">
-     
- <img  src="emerald.png"> 
- <img src="prince.jpg">
-
-   <img src="vodafone.jpg">
-  
-</div> 
-
-
-</div>
-         -->
          <footer class="footer">
     <div class="footer-item"> 
         <p>Volunteer</p>

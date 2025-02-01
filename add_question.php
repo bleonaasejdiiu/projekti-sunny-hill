988e14a7c91ceac57fa789bca034970e1cf312a4
bleonaasejdiiu <bleonasejdiu51@gmail.com>
2025-02-01 23:24:16 +0100

    shtim ne kod

--- add_question.php
+++ add_question.php
@@ -14,3 +14,10 @@ if (!$conn) {
 }
 
 $admin = new Admin($conn);
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    // Merr të dhënat nga formulari
+    $name = $_POST['name'];
+    $surname = $_POST['surname'];
+    $age = $_POST['age'];
+    $email = $_POST['email'];
+    $question = $_POST['question'];

0aa38d635b8f92ab25f3d2cce9bdb529de6ac6d4
ertaismajlii <ertaismajli@hotmail.com>
2025-02-02 23:33:07 +0100

    ndryshime validimi

--- Admin/edit_user.php
+++ Admin/edit_user.php
@@ -120,7 +120,7 @@ if (isset($_GET['id'])) {
     <div class="form-container">
         <h2>Edit User</h2>
 
-        <form method="POST" action="edit_user.php">
+        <form method="POST" action="edit_user.php"  onsubmit="return validateRegisterForm()">
             <input type="hidden" name="id" value="<?php echo $user['Id']; ?>">
 
             <label for="username">Username</label>
@@ -140,6 +140,33 @@ if (isset($_GET['id'])) {
             <a href="../admin_dashboard.php">Cancel</a>
         </div>
     </div>
+
+    <script>
+        function validateRegisterForm() {
+    const username = document.getElementById("username").value;
+    const email = document.getElementById("email").value;
+    const age = document.getElementById("age").value;
+   
+
+    if (username === "" || email === "" || age === "" || password === "") {
+        alert("Please fill in all fields.");
+        return false;
+    }
+
+    const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+    if (!emailPattern.test(email)) {
+        alert("Please enter a valid email.");
+        return false;
+    }
+
+    if (age < 18) {
+        alert("You must be at least 18 years old to register.");
+        return false;
+    }
+
+    return true;
+}
+    </script>
 
 </body>
 </html>
